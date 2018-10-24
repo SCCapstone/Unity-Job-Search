fe22a13b89f4e1b54a3a452f067d3a42501b239d
BROOKS <brookrl@email.sc.edu>
2018-10-24 10:20:19 -0400

    -Added dorm room template. -Added Temporary Movement Script. -Lost teleportation scene and dialogue scene.

---
+++ Unity-Job-Search/Assets/VRSampleScenes/Scenes/DemoMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+// Tilt the cube using the arrow keys.  When the arrow keys are released
+// the cube will be rotated back to the center using Slerp.
+
+public class DemoMovement : MonoBehaviour
+{
+
+
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.left);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(Vector3.right);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up);
+        }
+    }
+
+}
