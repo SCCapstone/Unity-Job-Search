09915c2799dd86def31fc5db4a262ecdbc28e067
Matthew Rickabaugh <42723775+rickabm@users.noreply.github.com>
2018-11-07 23:32:46 -0500

    Added 3 Scripts: Tooltip, Player, and Controller

---
+++ Unity-Job-Search/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    public Vector2 joystick;
+    public float speed;
+    public GameObject centerEye;
+    public GameObject pObject;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        joystick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+        transform.eulerAngles = new Vector3(0, centerEye.transform.localEulerAngles.y, 0);
+        transform.Translate(Vector3.forward * speed * joystick.y * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * joystick.x * Time.deltaTime);
+
+        pObject.transform.position = Vector3.Lerp(pObject.transform.position, transform.position, 10f * Time.deltaTime);
+	}
+}
