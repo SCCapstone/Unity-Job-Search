b1c048764b61c7ee45bd3cf03518c50fda0ae8dc
Rukia Brooks <rukiab@live.com>
2018-11-16 12:46:23 -0500

    -Rukia Door Update.
    -Added Door.
    -Added Teleportation.
    -Added Main Scene for Main Menu Template.
    -Edited Player Code and added Pointing and Selecting functionality.

--- Unity-Job-Search/Assets/Scripts/PlayerScript.cs
+++ Unity-Job-Search/Assets/Scripts/PlayerScript.cs
@@ -1,24 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour {
     public Vector2 joystick;
     public float speed;
-    public GameObject centerEye;
-    public GameObject pObject;
+    public GameObject centerEyeAnchor;
+    public GameObject cameraRig;
+    public GameObject oculusGoRemote;
+    public Text DisplayText;
+    public GameObject Door;
 
-	void Start () {
-		
-	}
+    public GameObject InterviewButton;
+
+    public GameObject FairButton;
+
+    public GameObject ResumeButton;
+
+    //public GameObject CareerCenterButton;
 	
-	void Update () {
+    void HandlePlayerMovement()
+    {
         joystick = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
 
-        transform.eulerAngles = new Vector3(0, centerEye.transform.localEulerAngles.y, 0);
+        transform.eulerAngles = new Vector3(0, centerEyeAnchor.transform.localEulerAngles.y, 0);
         transform.Translate(Vector3.forward * speed * joystick.y * Time.deltaTime);
         transform.Translate(Vector3.right * speed * joystick.x * Time.deltaTime);
 
-        pObject.transform.position = Vector3.Lerp(pObject.transform.position, transform.position, 10f * Time.deltaTime);
-	}
+        cameraRig.transform.position = Vector3.Lerp(cameraRig.transform.position, transform.position, 10f * Time.deltaTime);
+    }
+
+    void HandleGyroController()
+    {
+        oculusGoRemote.transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(oculusGoRemote.transform.position,transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+
+            if (hit.collider.gameObject.name == "door")
+            {
+                Door.GetComponent<DoorBehavior>().displayText();
+                if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger)==1)
+                {
+                    DisplayText.text = "Opened Door";
+                    Door.GetComponent<DoorBehavior>().openDoorMenu();
+                }
+            }
+            else if(hit.collider.gameObject.name=="Interview_Button")
+            {
+                InterviewButton.GetComponent<VRButtonBehavior>().changeColor();
+                DisplayText.text = "Button Touched";
+                SceneManager.LoadScene("Main_Scene");
+            }
+            else
+            {
+                Door.GetComponent<DoorBehavior>().hideText();
+                Door.GetComponent<DoorBehavior>().closeDoorMenu();
+                InterviewButton.GetComponent<VRButtonBehavior>().resetColor();
+
+            }
+
+
+            Debug.DrawLine(oculusGoRemote.transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.cyan);
+            Debug.Log("Did Hit");
+        }
+    }
+
+	void Update ()
+    {
+        HandlePlayerMovement();
+        HandleGyroController();
+
+    }
 }
---
+++ Unity-Job-Search/Assets/Scripts/VRButtonBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRButtonBehavior : MonoBehaviour {
+
+    public void changeColor()
+    {
+        GetComponent<Image>().color = Color.cyan;
+    }
+
+    public void resetColor()
+    {
+        GetComponent<Image>().color = Color.white;
+
+    }
+
+}
