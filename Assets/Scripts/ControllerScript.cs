ef0dc484de454bb904872bfa750c71457b393077
Rukia Brooks <rukiab@live.com>
2018-11-16 03:25:46 -0500

    -Teleport Door and Menu Added.
    -Created teleport door.
    -Created 3D Teleport Menu.
    -Cleaned up Player Code by separating into two distinct functions (Movement, and Controller Input)
    -Changed Debug Ray to Cyan.
    -Added panoramic test image.

---
+++ Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour {
+
+    public GameObject go;
+    public GameObject empty;
+
+	void Start () {
+        empty = new GameObject();
+        go = empty;
+	}
+	
+	void Update () {
+        RaycastHit hit;
+        transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+
+        if(Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if(hit.collider != null)
+            {
+                if(go != hit.collider.gameObject)
+                {
+                    go.transform.SendMessage("OnVRExit");
+                    go = hit.transform.gameObject;
+                    go.transform.SendMessage("OnVREnter");
+                    Debug.Log("On VR Raycast Enter");
+                }
+            }
+        }
+        else
+        {
+            if(go != null)
+            {
+                go.transform.SendMessage("OnVRExit");
+                go = empty;
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/TooltipScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipScript : MonoBehaviour {
+
+    public Text helpText;
+    public Text tooltipText;
+
+    public string helpString;
+    public string tooltipString;
+
+	void Start () {
+        helpText = GameObject.Find("HelpText").GetComponent<Text>();
+        tooltipText = GameObject.Find("TooltipText").GetComponent<Text>();
+    }
+
+	void Update () {
+		
+	}
+
+    void OnVREnter()
+    {
+        helpText.text = helpString;
+        tooltipText.text = tooltipString;
+
+    }
+    void OnVRExit()
+    {
+        helpText.text = "";
+        tooltipText.text = "";
+    }
+}
