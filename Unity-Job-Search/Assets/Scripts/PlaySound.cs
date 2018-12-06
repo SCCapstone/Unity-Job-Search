c9cb9e8113622a1d8c141554673ac822a58b62f6
Hung Nguyen <hungvnguyen0797@gmail.com>
2018-12-06 18:06:21 -0500

    Audio OneShot at CareerCenter working

---
+++ Unity-Job-Search/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+    public AudioClip sound;
+    public float Volume;
+    AudioSource audio;
+    public bool playedAlready = false;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!playedAlready)
+        {
+            audio.PlayOneShot(sound, Volume);
+            playedAlready = true;
+        }
+	}
+}
